c95dd61adab59b328314e0647dff11752320fa85
Chuck Skittle <chaz@Chucks-MacBook-Pro.local>
2026-06-03 22:59:53 +0900

    Adding proper tournement import

--- cryptoball-app/src/contracts/tournementContract.ts
+++ cryptoball-app/src/contracts/tournementContract.ts
@@ -1,6 +1,6 @@
 import { Contract } from "./types";
 import { getContractAddress } from "../config/network";
-import tournementArtifact from "../../../smart-contracts/artifacts/contracts/Tournement.sol/Tournement.json";
+import tournementArtifact from "./abi/Tournement.json";
 
 export const tournementContract: Contract = {
   address: getContractAddress("tournement"),
